49759b23e3c9ee3dcf297e32813acc963c85ec92
Vasily E <laer.18@gmail.com>
2019-01-08 16:07:17 +0300

    ASyncActivity: simple SMS reading
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- src/edu/real/async/android/ASyncActivity.java
+++ src/edu/real/async/android/ASyncActivity.java
@@ -140,6 +140,26 @@ public class ASyncActivity extends FragmentActivity
                     SELECTION_RAW, mSelectionArgs, SORT_ORDER);
         }
 
+        // Refs: https://stackoverflow.com/questions/848728/how-can-i-read-sms-messages-from-the-device-programmatically-in-android/32153303
+        // public static final String INBOX = "content://sms/inbox";
+        // public static final String SENT = "content://sms/sent";
+        // public static final String DRAFT = "content://sms/draft";
+        Cursor cursor = getContentResolver().query(
+                Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        if (cursor.moveToFirst()) { // must check the result to prevent
+                                    // exception
+            do {
+                String msgData = "";
+                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                    msgData += " " + cursor.getColumnName(idx) + ":"
+                            + cursor.getString(idx);
+                }
+                // use msgData
+            } while (cursor.moveToNext());
+        } else {
+            // empty box, no SMS
+        }
     }
 
     @Override
