3b214a53f2ac6c89bf45f7e8ad591f63ac2d2b38
Vasily E <laer.18@gmail.com>
2019-01-08 18:41:56 +0300

    simple SMS (inbox) sending
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- src/edu/real/async/android/ASyncActivity.java
+++ src/edu/real/async/android/ASyncActivity.java
@@ -100,7 +100,7 @@ public class ASyncActivity extends FragmentActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new SMSSenderThread().start();
+        new SMSSenderThread(this).start();
 
         setContentView(R.layout.activity_async);
 
@@ -141,27 +141,6 @@ public class ASyncActivity extends FragmentActivity
             CursorLoader mLoader = new CursorLoader(this,
                     ContactsContract.Data.CONTENT_URI, PROJECTION_RAW,
                     SELECTION_RAW, mSelectionArgs, SORT_ORDER);
-        }
-
-        // Refs: https://stackoverflow.com/questions/848728/how-can-i-read-sms-messages-from-the-device-programmatically-in-android/32153303
-        // public static final String INBOX = "content://sms/inbox";
-        // public static final String SENT = "content://sms/sent";
-        // public static final String DRAFT = "content://sms/draft";
-        Cursor cursor = getContentResolver().query(
-                Uri.parse("content://sms/inbox"), null, null, null, null);
-
-        if (cursor.moveToFirst()) { // must check the result to prevent
-                                    // exception
-            do {
-                String msgData = "";
-                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
-                    msgData += " " + cursor.getColumnName(idx) + ":"
-                            + cursor.getString(idx);
-                }
-                // use msgData
-            } while (cursor.moveToNext());
-        } else {
-            // empty box, no SMS
         }
     }
 
--- src/edu/real/async/android/SMSSenderThread.java
+++ src/edu/real/async/android/SMSSenderThread.java
@@ -9,9 +9,28 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SMSSenderThread extends Thread {
+
+    private static final int STATE_G_TEST_SENT = 1;
+    private static final int STATE_CONNECTED = 0;
+    private static final int STATE_ID_SENT = 2;
+    private Context ctx;
+    private int state;
+    private PrintWriter sOut;
+    private BufferedReader sIn;
+    private boolean working;
+
+    public SMSSenderThread(Context _ctx) {
+        ctx = _ctx;
+    }
 
     @Override
     public void run() {
@@ -26,21 +45,21 @@ public class SMSSenderThread extends Thread {
 
             Socket ss = new Socket(serverAddr, 4321);
 
-            // sends the message to the server
-            PrintWriter sOut = new PrintWriter(
+            sOut = new PrintWriter(
                     new BufferedWriter(
                             new OutputStreamWriter(ss.getOutputStream())),
                     true);
 
-            // receives the message which the server sends back
-            BufferedReader sIn = new BufferedReader(
+            sIn = new BufferedReader(
                     new InputStreamReader(ss.getInputStream()));
 
-            int state = 0;
-            sOut.println("G");
-            state = 1;
+            state = STATE_CONNECTED;
+            // sOut.println("G");
+            // state = STATE_G_TEST_SENT;
+            sOut.println("Iinnosd6000#1");
+            state = STATE_ID_SENT;
 
-            boolean working = true;
+            working = true;
             while (working) {
                 String msg = sIn.readLine();
 
@@ -49,10 +68,20 @@ public class SMSSenderThread extends Thread {
                 }
 
                 switch (state) {
-                case 1:
+                case STATE_G_TEST_SENT:
                     if (!"g".equals(msg)) {
                         Log.e("protocol", "Unexpected server response " + msg);
                     }
+                    working = false;
+                    break;
+                case STATE_ID_SENT:
+                    if (!"i".contentEquals(msg)) {
+                        Log.e("protocol",
+                                "Identification failed with message " + msg);
+                        working = false;
+                        break;
+                    }
+                    sendSMS();
                     working = false;
                     break;
                 }
@@ -63,5 +92,61 @@ public class SMSSenderThread extends Thread {
             Log.e("net", e.getMessage());
         }
 
+    }
+
+    private void sendSMS() {
+        // Refs: https://stackoverflow.com/questions/848728/how-can-i-read-sms-messages-from-the-device-programmatically-in-android/32153303
+        // public static final String INBOX = "content://sms/inbox";
+        // public static final String SENT = "content://sms/sent";
+        // public static final String DRAFT = "content://sms/draft";
+        Cursor cursor = ctx.getContentResolver().query(
+                Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        if (cursor.moveToFirst()) { // must check the result to prevent
+                                    // exception
+            do {
+                JSONObject o_sms = new JSONObject();
+                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                    try {
+                        o_sms.put(cursor.getColumnName(idx),
+                                cursor.getString(idx));
+                    } catch (JSONException e) {
+                        Log.e("sms-send", "Putting to JSONObject failed. "
+                                + e.getMessage());
+                        o_sms = null;
+                        break;
+                    }
+                }
+                if (o_sms == null) {
+                    continue;
+                }
+                sOut.println("S" + o_sms.toString());
+                String resp;
+                try {
+                    resp = sIn.readLine();
+                } catch (IOException e) {
+                    Log.e("sms-send", "Server response reading failed. "
+                            + e.getMessage());
+                    working = false;
+                    break;
+                }
+                if (resp == null) {
+                    Log.e("sms-send",
+                            "Server responsed nothing after SMS sent. "
+                            + resp);
+                    working = false;
+                    break;
+                }
+                if (!resp.equals("s")) {
+                    Log.e("sms-send",
+                            "Unexpected server response after SMS sent. "
+                            + resp);
+                    working = false;
+                    break;
+                }
+            } while (cursor.moveToNext());
+        } else {
+            // empty box, no SMS
+        }
     }
 }
