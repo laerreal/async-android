fc05cd58212e7eff71400f53fd95b7bd5847e3a5
Vasily E <laer.18@gmail.com>
2019-01-11 18:00:06 +0300

    CallsSenderThread, SMSSenderThread: use SenderThread's helpers to send data
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- src/edu/real/async/android/SMSSenderThread.java
+++ src/edu/real/async/android/SMSSenderThread.java
@@ -1,14 +1,6 @@
 package edu.real.async.android;
 
-import java.io.IOException;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.util.Log;
 
 public class SMSSenderThread extends SenderThread {
 
@@ -21,54 +13,7 @@ public class SMSSenderThread extends SenderThread {
         // public static final String INBOX = "content://sms/inbox";
         // public static final String SENT = "content://sms/sent";
         // public static final String DRAFT = "content://sms/draft";
-        Cursor cursor = ctx.getContentResolver().query(
-                Uri.parse("content://sms"), null, null, null, null);
 
-        if (cursor.moveToFirst()) { // must check the result to prevent
-                                    // exception
-            do {
-                JSONObject o_sms = new JSONObject();
-                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
-                    try {
-                        o_sms.put(cursor.getColumnName(idx),
-                                cursor.getString(idx));
-                    } catch (JSONException e) {
-                        Log.e("sms-send", "Putting to JSONObject failed. "
-                                + e.getMessage());
-                        o_sms = null;
-                        break;
-                    }
-                }
-                if (o_sms == null) {
-                    continue;
-                }
-                sOut.println("S" + o_sms.toString());
-                String resp;
-                try {
-                    resp = sIn.readLine();
-                } catch (IOException e) {
-                    Log.e("sms-send", "Server response reading failed. "
-                            + e.getMessage());
-                    working = false;
-                    break;
-                }
-                if (resp == null) {
-                    Log.e("sms-send",
-                            "Server responsed nothing after SMS sent. "
-                            + resp);
-                    working = false;
-                    break;
-                }
-                if (!resp.equals("s")) {
-                    Log.e("sms-send",
-                            "Unexpected server response after SMS sent. "
-                            + resp);
-                    working = false;
-                    break;
-                }
-            } while (cursor.moveToNext());
-        } else {
-            // empty box, no SMS
-        }
+        sendAllContent("content://sms", "S", "s", "SMS", "sms-send");
     }
 }
